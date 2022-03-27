7fd21b4e32329c848daf9777dc4906cd69ec6326
dzy <ziyingdeng9709@gmail.com>
2022-03-27 08:38:32 -0700

    1. Added logger. 2. Implemented running and result checking. 3. Multithreading.

--- src/main/java/com/oni/web/controller/ProgramController.java
+++ src/main/java/com/oni/web/controller/ProgramController.java
@@ -3,6 +3,8 @@ package com.oni.web.controller;
 import com.oni.web.model.MatlabEngineManager;
 import com.oni.web.model.Program;
 import com.mathworks.engine.MatlabEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -21,21 +25,17 @@ import java.util.concurrent.ExecutionException;
 @RestController
 @RequestMapping("/program")
 public class ProgramController {
-    @Value("${matlab_program_path}")
+    @Value("${matlab_program_root_path}${matlab_program_rel_dir}")
     String programPath;
+    @Value("${matlab_program_root_path}${matlab_program_progress_rel_dir}")
+    String progressPath;
+    @Value("${matlab_program_root_path}${matlab_program_output_rel_dir}")
+    String outputPath;
 
+    Logger logger = LoggerFactory.getLogger(ProgramController.class);
     @GetMapping("/start/{programName}")
-    public ResponseEntity startProgram(@PathVariable String programName) {
-        try {
-            MatlabEngine ml = MatlabEngineManager.getEngine(programName);
-            //add path
-            ml.feval("addpath", programPath);
-            int res = ml.feval(programName, 1, 2);
-            System.out.println("Start matlab program: " + programName + "res: " + res);
-
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public ResponseEntity<String> startProgram(@PathVariable String programName) {
+        MatlabEngineManager.runProgram(programPath, outputPath, programName);
         return ResponseEntity.ok().build();
 //        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
     }
@@ -43,7 +43,7 @@ public class ProgramController {
     @GetMapping("/pause/{programName}")
     public void pauseProgram(@PathVariable String programName) {
         // command process id terminal ctrl+Z
-        System.out.println("pause matlab program: " + programName);
+        logger.info("pause matlab program: " + programName);
 
 //        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
     }
@@ -51,19 +51,28 @@ public class ProgramController {
     @GetMapping("/terminate/{programName}")
     public void terminateProgram(@PathVariable String programName) {
         // command   ctrl+ C
-        System.out.println("terminate matlab program: " + programName);
+        logger.info("terminate matlab program: " + programName);
 
 //        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
+    //    Check the output of the program and return it, return null if not finished.
     @GetMapping("/checkResult/{programName}")
-    public void checkProgram(@PathVariable String programName) {
-        // command
-        MatlabEngine ml = MatlabEngineManager.getEngine(programName);
+    public ResponseEntity<Object> checkProgram(@PathVariable String programName) {
 
-        System.out.println("checkResult matlab program: " + programName);
-
-//        return clientRepository.findById(id).orElseThrow(RuntimeException::new);
+        Path path = Path.of(outputPath + programName);
+        logger.info("checkResult matlab program: " + programName);
+        if(!Files.exists(path)){
+            logger.warn("output file doesn't exist. Check later.");
+            return ResponseEntity.ok("Still running, check later");
+        }
+        String result = null;
+        try {
+            result = Files.readString(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/")
--- src/main/java/com/oni/web/model/MatlabEngineManager.java
+++ src/main/java/com/oni/web/model/MatlabEngineManager.java
@@ -1,31 +1,100 @@
 package com.oni.web.model;
 
+import com.mathworks.engine.EngineException;
 import com.mathworks.engine.MatlabEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+
+class ProgramRunner implements Runnable {
+    private MatlabEngine engine;
+    private String programPath;
+    private String programName;
+    private String outputPath;
+    private Logger logger = LoggerFactory.getLogger(Runnable.class);
+
+    public ProgramRunner(MatlabEngine engine, String programPath, String outputPath, String programName) {
+        this.engine = engine;
+        this.programPath = programPath;
+        this.programName = programName;
+        this.outputPath = outputPath;
+
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            var path = Path.of(outputPath + programName);
+            double res = engine.feval(programName);
+            Path file = Files.createFile(path);
+            Files.writeString(file, String.valueOf(res), StandardCharsets.UTF_8);
+        } catch (InterruptedException | ExecutionException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
 public class MatlabEngineManager {
 
 
-    private static HashMap<String, MatlabEngine> map = new HashMap<String, MatlabEngine>();
+    private static HashMap<String, MatlabEngine> engineMap = new HashMap<String, MatlabEngine>();
+    private static HashMap<String, Thread> threadMap = new HashMap<String, Thread>();
 
-    public static MatlabEngine getEngine(String programName) {
-        boolean flagExist = map.containsKey(programName);
-        MatlabEngine returnMl = null;
-        if (flagExist) {
-            returnMl = map.get(programName);
+    private static Logger logger = LoggerFactory.getLogger(MatlabEngineManager.class);
+
+
+    public static void runProgram(String programPath, String outputPath, String programName) {
+        if (threadMap.containsKey(programName) && !threadMap.get(programName).isAlive()) {
+            threadMap.remove(programName);
+        }
+        if (threadMap.containsKey(programName) && threadMap.get(programName).isAlive()) {
+            return;
+        }
+        // process starts
+        var path = Path.of(outputPath + programName);
+        try {
+            // This runs before getting the engine at the very beginning. Otherwise, could check the result of last run.
+            Files.deleteIfExists(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MatlabEngine engine = null;
+        if (engineMap.containsKey(programName)) {
+            engine = engineMap.get(programName);
         } else {
             try {
-                Future<MatlabEngine> eng = MatlabEngine.startMatlabAsync();
-                returnMl = eng.get();
-                map.put(programName,returnMl);
-            } catch (InterruptedException | ExecutionException e) {
+                engine = MatlabEngine.startMatlab();
+                engineMap.put(programName, engine);
+            } catch (EngineException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        return returnMl;
+        if (engine == null) {
+            logger.error("Engine instance is null!");
+            return;
+        }
+        try {
+            engine.feval("addpath", programPath);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        var runner = new ProgramRunner(engine, programPath, outputPath, programName);
+        Thread thread = new Thread(runner);
+        thread.start();
+        threadMap.put(programName, thread);
+
+
     }
 
 //    public MatlabEngine deleteEngine(String programName){
